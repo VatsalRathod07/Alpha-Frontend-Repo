8cb0524c1b8caeda34ab4d78b6d84d96be624ed2
VatsalRathod07 <vatsalrathod2005@gmail.com>
2023-08-04 13:53:34 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "@/components/Home";
 import About from "@/components/About";
 import PricingSection from "@/components/pricing-section";
+import Product from "@/components/Product";
 
 const Page = () => {
   return (

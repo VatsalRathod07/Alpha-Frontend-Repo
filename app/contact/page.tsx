d4e1b189ffd6f726dfe7ea737bfdb3df14dd1385
darshan <vdarshan613@gmail.com>
2023-08-07 09:11:17 +0530

    contact form created

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import { BsInstagram, BsPinterest, BsFacebook } from "react-icons/bs";
-import { BiLogoGmail, BiMap } from "react-icons/bi";
-import { IoCall } from "react-icons/io5";
 
 const Contact = () => {
   return (
@@ -17,74 +14,53 @@ const Contact = () => {
           Contact US
         </div>
       </div>
-      <section className="container pt-20 pb-6">
-        <div className="flex flex-col-reverse items-center sm:flex-row justify-around gap-5 p-10">
-          <div className="flex flex-col gap-5">
-            <h2 className="text-4xl font-signature font-semibold text-center sm:text-start">
-              Get In Touch
-            </h2>
-            <p className="max-w-[480px] text-base text-center sm:text-start">
-              Sit vulputate faucibus eget eget scelerisque faucibus malesuada
-              nullam mollis ut montes, dui scelerisque ornare.
-            </p>
-            <div className="flex flex-col gap-2">
-              <div className="flex flex-col sm:flex-row gap-2 items-center">
-                <BiMap size={20} className="text-primary" />
-                {/* <p className="font-signature font-bold text-primary text-lg">VISIT US</p> */}
-                <p className="text-base text-center sm:text-start font-medium text-primary">
-                  123 Demo St, San Francisco, CA 45678, United States
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col gap-2">
-              <div className="flex flex-col sm:flex-row gap-2 items-center">
-                <IoCall size={20} className="text-primary" />
-                {/* <p className="font-signature font-bold text-primary text-lg">CALL US</p> */}
-                <p className="text-base text-center sm:text-start font-medium text-primary">
-                  +91 123-456-7890
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col gap-2">
-              <div className="flex flex-col sm:flex-row gap-2 items-center">
-                <BiLogoGmail size={20} className="text-primary" />
-                {/* <p className="font-signature font-bold text-primary text-lg">EMAIL US</p> */}
-                <p className="text-base text-center sm:text-start font-medium text-primary">
-                  alpha@gmail.com
-                </p>
-              </div>
-            </div>
+      <div className="text-center py-16 text-3xl font-semibold">
+        Get In Touch
+      </div>
+      <div className="contact-form max-w-[1000px] mt-0 mb-8 mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            className="border-0 rounded outline-0 h-[60px] w-full bg-[#f4f8fc] pl-4"
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            className="border-0 rounded outline-0 h-[60px] w-full bg-[#f4f8fc] pl-4"
+          />
 
-            <div className="flex flex-col gap-2 items-center sm:items-start">
-              <p className="font-signature font-bold text-primary text-center sm:text-start">
-                FOLLOW US
-              </p>
-              <div className="flex gap-5 text-primary">
-                <BsInstagram
-                  size={25}
-                  className="text-primary hover:text-secondary cursor-pointer"
-                />
-                <BsPinterest
-                  size={25}
-                  className="text-primary hover:text-secondary cursor-pointer"
-                />
-                <BsFacebook
-                  size={25}
-                  className="text-primary hover:text-secondary cursor-pointer"
-                />
-              </div>
-            </div>
-          </div>
-
-          <div>
-            <img
-              src="https://i.pinimg.com/736x/b9/b1/88/b9b18859a4ad4702ba076b2b0f118b92.jpg"
-              alt=""
-              className="w-[400px] h-[400px] object-cover"
-            />
-          </div>
+          <input
+            type="text"
+            name="phone"
+            placeholder="Phone"
+            className="border-0 rounded outline-0 h-[60px] w-full bg-[#f4f8fc] pl-4"
+          />
+          <input
+            type="text"
+            name="subject"
+            placeholder="Subject"
+            className="border-0 rounded outline-0 h-[60px] w-full bg-[#f4f8fc] pl-4"
+          />
         </div>
-      </section>
+        <div className="mt-4">
+          <textarea
+            name=""
+            id=""
+            cols={30}
+            rows={10}
+            placeholder="Message"
+            className="border-0 rounded outline-0 h-[80px] w-full bg-[#f4f8fc] pl-4 pt-4"
+          ></textarea>
+        </div>
+        <div className="text-center mt-4">
+          <button className="h-12 bg-light-purple text-lg text-white px-12 rounded-lg hover:bg-red-600 font-medium transition duration-300 ease-in-out">
+            Submit
+          </button>
+        </div>
+      </div>
     </>
   );
 };
